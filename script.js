3dfcb0ce6c5682c4b14912b0233326df7d12952c
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-06 09:53:05 +0500

    For Loop

---
+++ script.js
@@ -0,0 +1,149 @@
+// FOR LOOP INTRO TASKS ||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// TASK 1 |||||||||||||||||||||
+
+// let k = 1011;
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//     console.log(k);
+// }
+ 
+
+
+// TASK 2 |||||||||||||||||||||
+
+// Range olish
+// let a = 5; // boshlang'ich qiymat 
+// let b = 10; // tugash qiymat
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//     count += 1;
+//     console.log(i);
+// }
+// console.log(`${a} va ${b} sonlari orasidagi butun sonlar soni: ${count}`);
+
+
+
+// TASK 3 |||||||||||||||||||||
+
+// let a = 15;
+// let b = 5;
+// let count = 0;
+
+// for (let i = a - 1; i > b; i--) {
+//     count += 1;
+//     console.log(i);
+// }
+// console.log(`${a} va ${b} sonlar orasidagi butun sonlar soni: ${count}`);
+
+
+
+// TASK 4 |||||||||||||||||||||
+
+// let sweet = 400;
+// let kg = 10;
+
+// for (let i = 1; i <= kg; i++) {
+//     console.log(`${i}kg = $${sweet * i}`)
+// }
+
+
+
+// TASK 5 |||||||||||||||||||||
+
+// let sweet = 5000;
+// let kg = 1;
+
+// for (let i = 0.1; i <= kg; i += 0.1) {
+//     console.log(`${i}kg = $${Math.round(sweet * i)}`);
+// }
+
+
+
+// TASK 6 |||||||||||||||||||||
+
+// let sweet = 5000;
+// let kg = 2;
+
+// for (let i = 1.2; i <= kg; i += 0.2){
+//     console.log(`${i}kg = $${Math.round(i * sweet)}`);
+// }
+
+
+
+
+
+
+
+
+
+// BOOLEAN TASKS ||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// TASK 1 |||||||||||||||||||||
+
+// let a = 21;
+// let b = 12;
+
+// if (a % 2 === 0 && b % 2 === 0 || a % 2 !== 0 && b % 2 !== 0){
+//     console.log(`Kiritilgan ikkala son ham juft yoki toq`, true);
+// } else {
+//     console.log(`Kiritilgan sonlar bir xil toq yoki juft emas`, false);
+// }
+
+
+
+// TASK 2 |||||||||||||||||||||
+
+// let a = 1;
+// let b = 10;
+// let c = 11;
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+// TASK 3 |||||||||||||||||||||
+
+// let a = 1;
+// let b = -10;
+// let c = 0;
+
+// if (a > 0 || b > 0 || c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+// TASK 4 |||||||||||||||||||||
+
+// let a = 10;
+// let b = -15;
+// let c = -20;
+
+// if (a > 0 && b < 0 && c < 0 || b > 0 && a < 0 && c < 0 || c > 0 && b < 0 && a < 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+// TASK 5 |||||||||||||||||||||
+
+// let a = -10;
+// let b = 15;
+// let c = 20;
+
+// if (a > 0 && b > 0 && c < 0 || a > 0 && c > 0 && b < 0 || b > 0 && c > 0 && a < 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
